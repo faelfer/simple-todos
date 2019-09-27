bb8bf7feb28b1576dbffbbed9ebda056ac2f6fb1
NYDino <rfapp00@gmail.com>
2019-09-27 10:10:07 -0300

    starting application

--- toDos.js
+++ toDos.js
@@ -1,1 +1,9 @@
-alert("funcionou");
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElemente = document.querySelector('#app button');
+
+var toDos = [
+	'Fazer café',
+	'Estudar JavaScript',
+	'Acessar comunidade da Rocketseat'
+]
